be0e8e20817146b7a16694fb23c12b59a2da29f3
Jonathan Lebon <jonathan@jlebon.com>
2020-02-06 20:30:49 -0500

    wip

--- utils.groovy
+++ utils.groovy
@@ -31,12 +31,6 @@ def shwrap_rc(cmds) {
 
 def get_annotation(bc, anno) {
     def bcYaml = readYaml(text: shwrap_capture("oc get buildconfig ${bc} -n ${env.PROJECT_NAME} -o yaml"))
-    println(bcYaml['metadata'])
-    println()
-    println(bcYaml['metadata']['annotations'])
-    println()
-    println(bcYaml['metadata']['annotations']["coreos.com/${anno}"])
-    println()
     return bcYaml['metadata']['annotations']["coreos.com/${anno}"]
 }
 
